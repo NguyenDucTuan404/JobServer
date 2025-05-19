3fc1f9d9d249988fd96751805edad72bad8284d9
Nguyen Duc Tuan <tuan292011@gmail.com>
2025-05-20 04:50:07 +0700

    first commit

---
+++ src/db/index.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+mongoose.set('strictQuery', false);
+
+module.exports.connect = async() =>{
+  try {
+    await mongoose.connect( process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true  } );
+    console.log( 'Connected Database' );
+  } catch ( err ) {
+    console.log(err)
+    throw new Error(err.message);
+  }
+}
